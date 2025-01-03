d844f6de36fa9e020a7250412a544492451df2b9
aleksandardevelops <119680237+aleksandardevelops@users.noreply.github.com>
2025-01-03 23:53:13 +0100

    Add for loop to satisfy all test cases in Jest

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,5 +1,12 @@
-const repeatString = function() {
-
+const repeatString = function (string, num) {
+  let userSaying = '';
+  if (num < 0) {
+    return 'ERROR';
+  }
+  for (let i = 0; i < num; i++) {
+    userSaying += string;
+  }
+  return userSaying;
 };
 
 // Do not edit below this line
