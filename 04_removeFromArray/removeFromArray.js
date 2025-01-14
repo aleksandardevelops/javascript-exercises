82c16db9b7157b1395281e5381938e5f072804bb
aleksandardevelops <119680237+aleksandardevelops@users.noreply.github.com>
2025-01-15 00:53:20 +0100

    Add the core functionality of the functions to remove from array using indexOf and splice methods

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,5 +1,10 @@
-const removeFromArray = function() {
+const removeFromArray = function (array, ...num) {
+  for (const arg of num) {
+    const index = array.indexOf(arg);
+    array.splice(index, 1);
+  }
+  return array;
 };
-
+console.log(removeFromArray([1, 2, 3, 4], 3, 2));
 // Do not edit below this line
 module.exports = removeFromArray;
