8b36a4c372ce7287798690ee6d9c9ae877227692
aleksandardevelops <119680237+aleksandardevelops@users.noreply.github.com>
2025-01-21 22:23:37 +0100

    Add for loop and conditional to solve the first 3 test cases

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,5 +1,16 @@
-const sumAll = function() {
+const sumAll = function (arg1, arg2) {
+  let agregator = 0;
+  if (arg1 < arg2) {
+    for (let i = arg1; i <= arg2; i++) {
+      agregator += i;
+    }
+  } else {
+    for (let i = arg1; i >= arg2; i--) {
+      agregator += i;
+    }
+  }
 
+  return agregator;
 };
 
 // Do not edit below this line
